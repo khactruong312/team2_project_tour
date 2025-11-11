8db5eb149082fe4cf2f304559cf0953b20027c92
khactruong312 <khactruong312205@gmail.com>
2025-11-11 20:37:56 +0700

    day1

--- configs/env.php
+++ configs/env.php
@@ -1,6 +1,6 @@
 <?php
 
-define('BASE_URL',          'http://localhost/DuAn1/Base1/');
+define('BASE_URL',          'http://project_dulich.test/');
 
 define('PATH_ROOT',         __DIR__ . '/../');
 
